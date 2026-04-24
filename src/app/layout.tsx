2de8917bd2c3a21ae4bf789fd7f55705b04b7582
Ezra <ezra@Seh-Pro.local>
2026-04-24 18:21:01 +0200

    Add Microsoft Clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import './globals.css'
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 const META_PIXEL_ID = '933186462850674'
+const CLARITY_ID = 'ryjdokg5a0'
 
 const dmSans = DM_Sans({
   subsets: ['latin'],
@@ -126,6 +127,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           async
           defer
         />
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_ID}");
+          `}
+        </Script>
 
         {/* Omnisend */}
         <Script id="omnisend-tracking" strategy="afterInteractive">
